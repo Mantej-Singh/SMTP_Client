4c63ab83740688fc6a24fe83216e33c3142b4734
Mantej-Singh <mdmantejsingh@gmail.com>
2016-05-14 17:22:01 -0400

    C#.Net
    Sending email through Gmail SMTP server using C#

---
+++ SMTP_Client/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace SMTP_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtpwd = new System.Windows.Forms.TextBox();
+            this.txtgid = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.textBoxSendTo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.btnsend = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBoxAttachment = new System.Windows.Forms.TextBox();
+            this.richTextBoxMessage = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtpwd
+            // 
+            this.txtpwd.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpwd.Location = new System.Drawing.Point(261, 39);
+            this.txtpwd.Name = "txtpwd";
+            this.txtpwd.PasswordChar = '*';
+            this.txtpwd.Size = new System.Drawing.Size(136, 20);
+            this.txtpwd.TabIndex = 1;
+            // 
+            // txtgid
+            // 
+            this.txtgid.Location = new System.Drawing.Point(261, 16);
+            this.txtgid.Name = "txtgid";
+            this.txtgid.Size = new System.Drawing.Size(136, 20);
+            this.txtgid.TabIndex = 0;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(189, 42);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(56, 13);
+            this.label12.TabIndex = 32;
+            this.label12.Text = "Password:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(189, 19);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(50, 13);
+            this.label13.TabIndex = 31;
+            this.label13.Text = "Gmail ID:";
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Location = new System.Drawing.Point(75, 37);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(308, 20);
+            this.textBoxSubject.TabIndex = 3;
+            // 
+            // textBoxSendTo
+            // 
+            this.textBoxSendTo.Location = new System.Drawing.Point(75, 13);
+            this.textBoxSendTo.Name = "textBoxSendTo";
+            this.textBoxSendTo.Size = new System.Drawing.Size(308, 20);
+            this.textBoxSendTo.TabIndex = 2;
+            this.textBoxSendTo.TextChanged += new System.EventHandler(this.textBoxSendTo_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Subject:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Recipients:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.pictureBox5);
+            this.panel2.Controls.Add(this.btnsend);
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.pictureBox6);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.textBoxAttachment);
+            this.panel2.Controls.Add(this.textBoxSubject);
+            this.panel2.Controls.Add(this.textBoxSendTo);
+            this.panel2.Controls.Add(this.richTextBoxMessage);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(12, 70);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(611, 269);
+            this.panel2.TabIndex = 35;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(-156, 235);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(840, 34);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 40;
+            this.pictureBox5.TabStop = false;
+            // 
+            // btnsend
+            // 
+            this.btnsend.Image = ((System.Drawing.Image)(resources.GetObject("btnsend.Image")));
+            this.btnsend.Location = new System.Drawing.Point(385, 12);
+            this.btnsend.Name = "btnsend";
+            this.btnsend.Size = new System.Drawing.Size(82, 35);
+            this.btnsend.TabIndex = 6;
+            this.btnsend.UseVisualStyleBackColor = true;
+            this.btnsend.Click += new System.EventHandler(this.btnsend_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(536, 13);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(37, 33);
+            this.pictureBox4.TabIndex = 39;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(504, 13);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(39, 33);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 41;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(468, 13);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(37, 33);
+            this.pictureBox3.TabIndex = 38;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(389, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(81, 33);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 37;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBoxAttachment
+            // 
+            this.textBoxAttachment.Location = new System.Drawing.Point(75, 61);
+            this.textBoxAttachment.Name = "textBoxAttachment";
+            this.textBoxAttachment.Size = new System.Drawing.Size(464, 20);
+            this.textBoxAttachment.TabIndex = 35;
+            // 
+            // richTextBoxMessage
+            // 
+            this.richTextBoxMessage.Location = new System.Drawing.Point(61, 98);
+            this.richTextBoxMessage.Name = "richTextBoxMessage";
+            this.richTextBoxMessage.Size = new System.Drawing.Size(464, 124);
+            this.richTextBoxMessage.TabIndex = 5;
+            this.richTextBoxMessage.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Message:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Attachment:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 340);
+            this.Controls.Add(this.txtpwd);
+            this.Controls.Add(this.txtgid);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Email Client";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtpwd;
+        private System.Windows.Forms.TextBox txtgid;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.TextBox textBoxSendTo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button btnsend;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBoxAttachment;
+        private System.Windows.Forms.RichTextBox richTextBoxMessage;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+
+    }
+}
+
---
+++ SMTP_Client/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;//library i used
+
+namespace SMTP_Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsend_Click(object sender, EventArgs e)
+        {
+            if (textBoxSendTo.Text == "")
+            {
+                MessageBox.Show("Please enter the Recipient's Name");
+            }
+
+                  try
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(textBoxSendTo.Text);
+                //mail.To.Add("mdmantejsingh@gmail.com");
+                mail.From = new MailAddress(txtgid.Text);
+                mail.Subject = textBoxSubject.Text;
+                string Body = richTextBoxMessage.Text;
+                mail.Body = Body;
+                if (textBoxAttachment.Text != "")
+                {
+                    mail.Attachments.Add(new Attachment(textBoxAttachment.Text));
+                }
+                mail.IsBodyHtml = true;
+
+
+                SmtpClient smtp = new SmtpClient("localhost", 587);
+                smtp.Host = "smtp.gmail.com"; //Or Your SMTP Server Address  
+                smtp.Credentials = new System.Net.NetworkCredential
+                     (txtgid.Text, txtpwd.Text);
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                MessageBox.Show("Message delivered successfully!!!", "Yaaayyyy...", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Message could not be sent.\nThe following error was returned:\n'" + ex.Message + "'", "SMTP Email Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+      
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textBoxAttachment.Text = openFileDialog1.FileName;
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            txtgid.Clear();
+            txtpwd.Clear();
+            textBoxSendTo.Clear();
+            textBoxSubject.Clear();
+            textBoxAttachment.Clear();
+            richTextBoxMessage.Clear();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fontDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Font font = fontDialog1.Font;
+                this.richTextBoxMessage.Font = font;
+            }
+        }
+
+        private void textBoxSendTo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
